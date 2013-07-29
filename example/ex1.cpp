da537c4b426338362119e88cf26e464cb825e89e
Marcin Zdun <mzdun@midnightbits.com>
2013-07-29 22:04:05 +0200

    Fix on the example

--- example/ex1.cpp
+++ example/ex1.cpp
@@ -11,13 +11,23 @@ using std::ext::reverse;
 
 int main(int argc, char* argv[])
 {
-	std::vector<std::string> poem {"Marry", "had", "a", "little", "lamb"};
+	std::vector<std::string> poem { "Marry", "had", "a", "little", "lamb" };
+
 	for (auto && s : poem)
-		std::cout << s << std::endl;
+		std::cout << s << " ";
+	std::cout << std::endl;
+
 	for (auto && s : reverse(poem))
-		std::cout << s << std::endl;
-	for (auto && s : make_range(poem.begin()+1, poem.end()))
-		std::cout << s << std::endl;
+		std::cout << s << " ";
+	std::cout << std::endl;
+
+	for (auto && s : make_range(poem.begin() + 1, poem.end()))
+		std::cout << s << " ";
+	std::cout << std::endl;
+
+	for (auto && s : make_range(poem.rbegin(), poem.rend() - 1))
+		std::cout << s << " ";
+	std::cout << std::endl;
 
 	int args [] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
